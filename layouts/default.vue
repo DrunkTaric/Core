8b0b49b6df7ccbca5a105bbdae25102a66010f35
comborush <comborush.init@proton.me>
2024-06-07 22:12:09 +0300

    adding next ui and layout

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const nav = "h"
+const login = false
+</script>
+
+<template>
+  <main class="flex flex-col w-full min-h-screen">
+    <Nav :variation="nav" />
+    <slot />
+  </main>
+</template>
