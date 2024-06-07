94b41f5a8f2ef99707af7c619e7e6737348e3473
comborush <comborush.init@proton.me>
2024-06-07 22:12:39 +0300

    profile layout

---
+++ layouts/profile.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const nav = "v"
+const login = false
+</script>
+
+<template>
+  <main class="flex w-full h-screen">
+    <Nav :variation="nav" />
+    <slot />
+  </main>
+</template>
